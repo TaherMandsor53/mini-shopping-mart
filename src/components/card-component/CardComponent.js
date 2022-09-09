f2d3c545661bec84224ca22c6d40c5087b2bc39d
Taher Mandsor <tahermandsorwala29@gmail.com>
2022-09-10 00:50:31 +0530

    Added useNavigate hook

--- src/components/card-component/CardComponent.js
+++ src/components/card-component/CardComponent.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Rating } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function CardComponent(props) {
   const { cardData, key } = props;
+  const navigate = useNavigate();
+
+  const onCardClick = useCallback(() => {
+    navigate(`/product-details/${cardData?.id}`);
+  }, [cardData?.id, navigate]);
 
   return (
-    <div className="card-details" key={key}>
-      <a href={`/product-details/${cardData?.id}`} rel="noreferrer">
-        <img src={cardData?.image} alt="card-img" className="card-img" />
-        <h3>{cardData?.title}</h3>
-        <p>{cardData?.description}</p>
-        <h4>${cardData?.price} total</h4>
-        <div className="card-rating">
-          <Rating readOnly value={cardData?.rating?.rate} />
-        </div>
-      </a>
+    <div className="card-details" key={key} onClick={onCardClick}>
+      <img src={cardData?.image} alt="card-img" className="card-img" />
+      <h3>{cardData?.title}</h3>
+      <p>{cardData?.description}</p>
+      <h4>${cardData?.price} total</h4>
+      <div className="card-rating">
+        <Rating readOnly value={cardData?.rating?.rate} />
+      </div>
     </div>
   );
 }
